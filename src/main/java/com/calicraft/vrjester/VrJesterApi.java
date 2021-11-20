40e0f3b9db7c44749c76ee0d0275c40f605b9c94
David Jake Morfe <jakemorfe@gmail.com>
2021-11-19 23:43:47 -0500

    0.0.1

--- src/main/java/com/calicraft/vrjester/VrJesterApi.java
+++ src/main/java/com/calicraft/vrjester/VrJesterApi.java
@@ -15,6 +15,8 @@ import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.vivecraft.api.VRData;
+
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
