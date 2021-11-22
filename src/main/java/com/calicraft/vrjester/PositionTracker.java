9af43882a3d2761b93ad85af7592023db8653daf
David Jake Morfe <jakemorfe@gmail.com>
2021-11-21 23:49:01 -0500

    0.0.3

--- src/main/java/com/calicraft/vrjester/PositionTracker.java
+++ src/main/java/com/calicraft/vrjester/PositionTracker.java
@@ -1,6 +1,144 @@
 package com.calicraft.vrjester;
 
-public class PositionTracker {
+import net.minecraft.util.math.vector.Vector3d;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.vivecraft.api.VRData;
+import org.vivecraft.utils.math.Matrix4f;
 
+import java.lang.reflect.*;
+
+public class PositionTracker {
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static Vector3d RC_POS, LC_POS;
+    public static Vector3d getRC() { // Get Right Controller Position
+        try {
+            Class<?> vrData = Class.forName("org.vivecraft.api.VRData");
+            System.out.println("HERE VRData");
+            Class<?>[] params = new Class[4];
+            params[0] = Vector3d.class;
+            params[1] = float.class;
+            params[2] = float.class;
+            params[3] = float.class;
+            Constructor<?> vrData_CT = vrData.getConstructor(params);
+            System.out.println("HERE Constructor 1");
+            Object[] args = new Object[4];
+            args[0] = new Vector3d(0,0,0);
+            args[1] = 0;
+            args[2] = 0;
+            args[3] = 0;
+            Object vrData_OBJ = vrData_CT.newInstance(args);
+            System.out.println(vrData_OBJ);
+            System.out.println("HERE Object 1");
+
+            Class<?>[] vrClasses = vrData.getDeclaredClasses();
+            System.out.println(vrClasses[0]);
+            Class<?> vrDevicePose = vrClasses[0];
+            System.out.println("HERE VRDevicePose");
+
+            Class<?>[] partypes = new Class[5];
+            partypes[0] = VRData.class;
+            partypes[1] = VRData.class;
+            partypes[2] = Matrix4f.class;
+            partypes[3] = Vector3d.class;
+            partypes[4] = Vector3d.class;
+//            System.out.println("HERE VRDevicePose Constructors");
+//            Constructor<?>[] cts = vrDevicePose.getConstructors();
+//            for (Constructor<?> ct: cts) {
+//                System.out.println(ct);
+//            }
+            Constructor<?> vrDevicePost_CT =  vrDevicePose.getConstructor(partypes);
+            vrDevicePost_CT.setAccessible(true);
+            System.out.println("HERE Constructor 2");
+            Object[] arglist = new Object[5];
+            arglist[0] = vrData_OBJ;
+            arglist[1] = vrData_OBJ;
+            arglist[2] = new Matrix4f();
+            arglist[3] = new Vector3d(0, 0, 0);
+            arglist[4] = new Vector3d(0, 0, 0);
+            Object vrDevicePose_OBJ = vrDevicePost_CT.newInstance(arglist);
+            System.out.println("HERE Object 2");
+            Method getPos = vrDevicePose.getMethod("getPosition");
+            getPos.setAccessible(true);
+            System.out.println("HERE getPosition");
+
+            for(int i=0; i < 20; i++) {
+                RC_POS = (Vector3d) getPos.invoke(vrDevicePose_OBJ);
+                System.out.println("RIGHT CONTROLLER POS: " + RC_POS.toString());
+            }
+            return RC_POS;
+        } catch (ClassNotFoundException | NoClassDefFoundError e) {
+            LOGGER.error("Failed to load Vivecraft class!");
+        } catch (NoSuchMethodException e) {
+            LOGGER.error("Failed to get Vivecraft method!");
+        } catch (InstantiationException e) {
+            LOGGER.error("Failed to instantiate Vivecraft class!");
+        } catch (InvocationTargetException e) {
+            LOGGER.error("Failed to invoke Vivecraft getPos!");
+        } catch (IllegalAccessException e) {
+            LOGGER.error("Failed to access Vivecraft attribute!");
+        }
+        return new Vector3d(0,0,0);
+    }
+    public static Vector3d getLC() { // Get Left Controller Position
+        try {
+            Class<?> vrData = Class.forName("org.vivecraft.api.VRData");
+            System.out.println("HERE VRData");
+            Class<?>[] params = new Class[4];
+            params[0] = Vector3d.class;
+            params[1] = float.class;
+            params[2] = float.class;
+            params[3] = float.class;
+            Constructor<?> vrData_CT = vrData.getConstructor(params);
+            System.out.println("HERE Constructor 1");
+            Object[] args = new Object[4];
+            args[0] = new Vector3d(0,0,0);
+            args[1] = 0;
+            args[2] = 0;
+            args[3] = 0;
+            Object vrData_OBJ = vrData_CT.newInstance(args);
+            System.out.println(vrData_OBJ);
+            System.out.println("HERE Object 1");
+
+            Class<?>[] vrClasses = vrData.getDeclaredClasses();
+            System.out.println(vrClasses[0]);
+            Class<?> vrDevicePose = vrClasses[0];
+            System.out.println("HERE VRDevicePose");
+
+            Class<?>[] partypes = new Class[5];
+            partypes[0] = VRData.class;
+            partypes[1] = VRData.class;
+            partypes[2] = Matrix4f.class;
+            partypes[3] = Vector3d.class;
+            partypes[4] = Vector3d.class;
+            Constructor<?> vrDevicePost_CT =  vrDevicePose.getConstructor(partypes);
+            vrDevicePost_CT.setAccessible(true);
+            System.out.println("HERE Constructor 2");
+            Object[] arglist = new Object[5];
+            arglist[0] = vrData_OBJ;
+            arglist[1] = vrData_OBJ;
+            arglist[2] = new Matrix4f();
+            arglist[3] = new Vector3d(0, 0, 0);
+            arglist[4] = new Vector3d(0, 0, 0);
+            Object vrDevicePose_OBJ = vrDevicePost_CT.newInstance(arglist);
+            System.out.println("HERE Object 2");
+            Method getPos = vrDevicePose.getMethod("getPosition");
+            getPos.setAccessible(true);
+            System.out.println("HERE getPosition");
+
+            return LC_POS;
+        } catch (ClassNotFoundException | NoClassDefFoundError e) {
+            LOGGER.error("Failed to load Vivecraft class!");
+        } catch (NoSuchMethodException e) {
+            LOGGER.error("Failed to get Vivecraft method!");
+        } catch (InstantiationException e) {
+            LOGGER.error("Failed to instantiate Vivecraft class!");
+        } catch (InvocationTargetException e) {
+            LOGGER.error("Failed to invoke Vivecraft getPos!");
+        } catch (IllegalAccessException e) {
+            LOGGER.error("Failed to access Vivecraft attribute!");
+        }
+        return new Vector3d(0,0,0);
+    }
 }
 
--- src/main/java/com/calicraft/vrjester/VrJesterApi.java
+++ src/main/java/com/calicraft/vrjester/VrJesterApi.java
@@ -2,7 +2,6 @@ package com.calicraft.vrjester;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.Minecraft;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
@@ -18,12 +17,13 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.vivecraft.api.VRData;
-import org.vivecraft.gameplay.VRPlayer;
+import org.vivecraft.utils.math.Matrix4f;
 
-import java.lang.reflect.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.stream.Collectors;
 
-import static org.vivecraft.gameplay.screenhandlers.GuiHandler.mc;
+import com.calicraft.vrjester.PositionTracker;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("vrjester")
@@ -34,53 +34,24 @@ public class VrJesterApi
     private boolean isVivecraftLoaded = false;
 
     public VrJesterApi() {
-        // Register the setup method for modloading
+        // Register the setup getPos for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
+        // Register the enqueueIMC getPos for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
+        // Register the processIMC getPos for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
+        // Register the doClientStuff getPos for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
-        try {
-//            Class<?> vrData = Class.forName("org.vivecraft.api.VRData");
-            Class<?> vrPlayer = Class.forName("org.vivecraft.gameplay.VRPlayer");
-            isVivecraftLoaded = true;
-            System.out.println(vrPlayer.getName() + " class has been loaded!");
-            Field[] vrpf = vrPlayer.getFields();
-            System.out.println("VRPlayer Field 1: " + vrpf[0].toString());
-            Field vrdata_room_pre = vrPlayer.getDeclaredField("vrdata_room_pre");
-            vrdata_room_pre.setAccessible(true);
-            System.out.println("HERE 1");
-            Field[] devices = vrdata_room_pre.getClass().getFields();
-            for (Field device : devices) {
-                System.out.println("VIVECRAFT FIELD: " + device);
-            }
-            Field right_controller = vrdata_room_pre.getClass().getDeclaredField("c0");
-            right_controller.setAccessible(true);
-            System.out.println("HERE 2");
-            Method getPos = right_controller.getClass().getMethod("getPosition");
-            getPos.setAccessible(true);
-            System.out.println("HERE 3");
-            Vector3d rc_pos = (Vector3d) getPos.invoke(right_controller);
-            System.out.println("RIGHT CONTROLLER POS: " + rc_pos.toString());
-//            mc.vrPlayer.vrdata_room_pre.c0.getPosition();
-//            VRPlayer.get().vrdata_room_pre.c0.getPosition();
-        } catch (ClassNotFoundException | NoClassDefFoundError e) {
-            LOGGER.error("Failed to load Vivecraft class!");
-        } catch (InvocationTargetException e) {
-            LOGGER.error("Failed to invoke Vivecraft method!");
-        } catch (IllegalAccessException e) {
-            LOGGER.error("Failed to access Vivecraft!");
-        } catch (NoSuchFieldException e) {
-            LOGGER.error("Failed to get Vivecraft field!");
-        } catch (NoSuchMethodException e) {
-            LOGGER.error("Failed to get Method Vivecraft!");
-        }
-
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        try {
+            Class<?> vrData = Class.forName("org.vivecraft.api.VRData");
+            isVivecraftLoaded = true;
+            System.out.println("Vivecraft has been loaded!");
+        } catch (ClassNotFoundException | NoClassDefFoundError e) {
+            LOGGER.error("Failed to load Vivecraft class!");
+        }
     }
 
     private void setup(final FMLCommonSetupEvent event) {
@@ -105,7 +76,7 @@ public class VrJesterApi
                 map(m->m.getMessageSupplier().get()).
                 collect(Collectors.toList()));
     }
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    // You can use SubscribeEvent and let the Event Bus discover getPoss to call
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
         // do something when the server starts
@@ -120,6 +91,7 @@ public class VrJesterApi
         public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
             // register a new block here
             LOGGER.info("HELLO from Register Block");
+            System.out.println(PositionTracker.getRC().toString());
         }
     }
 }
