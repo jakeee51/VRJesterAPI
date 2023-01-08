51b0179fb12b8f4a727a3209696ee06c6280ab16
Mostafa Damir <mad237@njit.edu>
2023-01-08 17:28:51 -0500

    Unit test being sent

--- src/test/java/com/calicraft/vrjester/GestureComponentTest.java
+++ src/test/java/com/calicraft/vrjester/GestureComponentTest.java
@@ -1,25 +1,19 @@
 package com.calicraft.vrjester;
 
-import com.calicraft.vrjester.gesture.GestureComponent;
-import net.minecraft.gametest.framework.GameTest;
-import net.minecraft.gametest.framework.GameTestHelper;
-import net.minecraftforge.gametest.GameTestHolder;
-import net.minecraftforge.gametest.PrefixGameTestTemplate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static com.calicraft.vrjester.VrJesterApi.MOD_ID;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-@GameTestHolder(MOD_ID)
-public class GestureComponentTest {
-    @GameTest
-    public static void exampleTest(GameTestHelper helper) {
-        System.out.println("MADE IT");
-        GestureComponent gestureComponent = null;
-        helper.succeed();
+class CalculatorTests {
+    @Test
+    @DisplayName("0 + 1 = 1")
+    boolean addsTwoNumbers() {
+        if (0 + 1 == 1){
+            return true;
+        }
+        return false;
     }
-
-    // Class name is not prepended, template name is not specified
-    // Template Location at 'modid:exampletest2'
-    @PrefixGameTestTemplate(false)
-    @GameTest
-    public static void exampleTest2(GameTestHelper helper) { /*...*/ }
 }
+
+
