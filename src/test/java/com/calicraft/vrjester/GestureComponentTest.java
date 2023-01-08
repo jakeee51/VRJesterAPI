7c00d952e40431a13d03397efe179dba7b02a1fa
Mostafa Damir <mad237@njit.edu>
2023-01-08 18:04:39 -0500

    JUnit test finalized

--- src/test/java/com/calicraft/vrjester/GestureComponentTest.java
+++ src/test/java/com/calicraft/vrjester/GestureComponentTest.java
@@ -4,11 +4,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
 
 class GestureComponentTest {
     @Test
     @DisplayName("0 + 1 = 1")
     void addsTwoNumbers() {
-        assertEquals(1, 1);
+        assertEquals(0, 1);
+    }
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    void add (int first, int second, int expectedResult){
+        first = 0;
+        second = 1;
+        expectedResult = first + second;
+        int Result = 2;
+        assertEquals(Result, expectedResult);
     }
 }
