e827ad9af20f858734faeeee67d6797f4618f32a
Mostafa Damir <89495314+mostafa-damir@users.noreply.github.com>
2023-01-15 20:29:43 -0500

    Added the remaining gesture tests (#5)

--- src/test/java/com/calicraft/vrjester/GestureComponentTest.java
+++ src/test/java/com/calicraft/vrjester/GestureComponentTest.java
@@ -54,13 +54,134 @@ class GestureComponentTest {
         List<GestureComponent> lcGesture = new ArrayList<>();
         Vec3 dir = new Vec3((0),(0),(0));
         HashMap<String, Integer> devices = new HashMap<>();
-        GestureComponent gestureComponent1 = new GestureComponent("LC", "forward",
+        GestureComponent gestureComponent1 = new GestureComponent("RC", "forward",
                 0, 0.0, dir, devices);
-        GestureComponent gestureComponent2 = new GestureComponent("RC", "forward",
+        GestureComponent gestureComponent2 = new GestureComponent("LC", "forward",
                 0, 0.0, dir, devices);
-        rcGesture.add(gestureComponent2);
-        lcGesture.add(gestureComponent1);
-        Gesture PushGesture = new Gesture(hmdGesture, rcGesture, lcGesture);
-        assertEquals("PUSH", recognition.recognize(PushGesture).get("gestureName"));
+        rcGesture.add(gestureComponent1);
+        lcGesture.add(gestureComponent2);
+        Gesture pushGesture = new Gesture(hmdGesture, rcGesture, lcGesture);
+        assertEquals("PUSH", recognition.recognize(pushGesture).get("gestureName"));
     }
+    @Test
+    void testPullGesture() {
+        checkDevConfig();
+        List<GestureComponent> hmdGesture = new ArrayList<>();
+        List<GestureComponent> rcGesture = new ArrayList<>();
+        List<GestureComponent> lcGesture = new ArrayList<>();
+        Vec3 dir = new Vec3((0),(0),(0));
+        HashMap<String, Integer> devices = new HashMap<>();
+        GestureComponent gestureComponent1 = new GestureComponent("RC", "back",
+                0, 0.0, dir, devices);
+        GestureComponent gestureComponent2 = new GestureComponent("LC", "back",
+                0, 0.0, dir, devices);
+        rcGesture.add(gestureComponent1);
+        lcGesture.add(gestureComponent2);
+        Gesture lowerGesture = new Gesture(hmdGesture, rcGesture, lcGesture);
+        assertEquals("PULL", recognition.recognize(lowerGesture).get("gestureName"));
+    }
+    @Test
+    void testLowerGesture() {
+        checkDevConfig();
+        List<GestureComponent> hmdGesture = new ArrayList<>();
+        List<GestureComponent> rcGesture = new ArrayList<>();
+        List<GestureComponent> lcGesture = new ArrayList<>();
+        Vec3 dir = new Vec3((0),(0),(0));
+        HashMap<String, Integer> devices = new HashMap<>();
+        GestureComponent gestureComponent1 = new GestureComponent("RC", "down",
+                0, 0.0, dir, devices);
+        GestureComponent gestureComponent2 = new GestureComponent("LC", "down",
+                0, 0.0, dir, devices);
+        rcGesture.add(gestureComponent1);
+        lcGesture.add(gestureComponent2);
+        Gesture lowerGesture = new Gesture(hmdGesture, rcGesture, lcGesture);
+        assertEquals("LOWER", recognition.recognize(lowerGesture).get("gestureName"));
+    }
+    @Test
+    void testRaiseGesture() {
+        checkDevConfig();
+        List<GestureComponent> hmdGesture = new ArrayList<>();
+        List<GestureComponent> rcGesture = new ArrayList<>();
+        List<GestureComponent> lcGesture = new ArrayList<>();
+        Vec3 dir = new Vec3((0),(0),(0));
+        HashMap<String, Integer> devices = new HashMap<>();
+        GestureComponent gestureComponent1 = new GestureComponent("RC", "up",
+                0, 0.0, dir, devices);
+        GestureComponent gestureComponent2 = new GestureComponent("LC", "up",
+                0, 0.0, dir, devices);
+        rcGesture.add(gestureComponent1);
+        lcGesture.add(gestureComponent2);
+        Gesture lowerGesture = new Gesture(hmdGesture, rcGesture, lcGesture);
+        assertEquals("RAISE", recognition.recognize(lowerGesture).get("gestureName"));
+    }
+    @Test
+    void testGrowGesture() {
+        checkDevConfig();
+        List<GestureComponent> hmdGesture = new ArrayList<>();
+        List<GestureComponent> rcGesture = new ArrayList<>();
+        List<GestureComponent> lcGesture = new ArrayList<>();
+        Vec3 dir = new Vec3((0),(0),(0));
+        HashMap<String, Integer> devices = new HashMap<>();
+        GestureComponent gestureComponent1 = new GestureComponent("RC", "right",
+                0, 0.0, dir, devices);
+        GestureComponent gestureComponent2 = new GestureComponent("LC", "left",
+                0, 0.0, dir, devices);
+        rcGesture.add(gestureComponent1);
+        lcGesture.add(gestureComponent2);
+        Gesture lowerGesture = new Gesture(hmdGesture, rcGesture, lcGesture);
+        assertEquals("GROW", recognition.recognize(lowerGesture).get("gestureName"));
+    }
+    @Test
+    void testShrinkGesture() {
+        checkDevConfig();
+        List<GestureComponent> hmdGesture = new ArrayList<>();
+        List<GestureComponent> rcGesture = new ArrayList<>();
+        List<GestureComponent> lcGesture = new ArrayList<>();
+        Vec3 dir = new Vec3((0),(0),(0));
+        HashMap<String, Integer> devices = new HashMap<>();
+        GestureComponent gestureComponent1 = new GestureComponent("RC", "left",
+                0, 0.0, dir, devices);
+        GestureComponent gestureComponent2 = new GestureComponent("LC", "right",
+                0, 0.0, dir, devices);
+        rcGesture.add(gestureComponent1);
+        lcGesture.add(gestureComponent2);
+        Gesture lowerGesture = new Gesture(hmdGesture, rcGesture, lcGesture);
+        assertEquals("SHRINK", recognition.recognize(lowerGesture).get("gestureName"));
+    }
+    @Test
+    void testBurstGesture() {
+        checkDevConfig();
+        List<GestureComponent> hmdGesture = new ArrayList<>();
+        List<GestureComponent> rcGesture = new ArrayList<>();
+        List<GestureComponent> lcGesture = new ArrayList<>();
+        Vec3 dir = new Vec3((0),(0),(0));
+        HashMap<String, Integer> devices = new HashMap<>();
+        GestureComponent gestureComponent1 = new GestureComponent("RC", "forward",
+                2000, 0.0, dir, devices);
+        GestureComponent gestureComponent2 = new GestureComponent("LC", "forward",
+                2000, 0.0, dir, devices);
+        rcGesture.add(gestureComponent1);
+        lcGesture.add(gestureComponent2);
+        Gesture lowerGesture = new Gesture(hmdGesture, rcGesture, lcGesture);
+        assertEquals("BURST", recognition.recognize(lowerGesture).get("gestureName"));
+    }
+    @Test
+    void testUppercutGesture() {
+        checkDevConfig();
+        List<GestureComponent> hmdGesture = new ArrayList<>();
+        List<GestureComponent> rcGesture = new ArrayList<>();
+        List<GestureComponent> lcGesture = new ArrayList<>();
+        Vec3 dir = new Vec3((0),(0),(0));
+        HashMap<String, Integer> devices = new HashMap<>();
+        GestureComponent gestureComponent1 = new GestureComponent("RC", "forward",
+                0, 0.0, dir, devices);
+        GestureComponent gestureComponent2 = new GestureComponent("RC", "up",
+                0, 0.0, dir, devices);
+        rcGesture.add(gestureComponent1);
+        rcGesture.add(gestureComponent2);
+        Gesture lowerGesture = new Gesture(hmdGesture, rcGesture, lcGesture);
+        assertEquals("UPPERCUT", recognition.recognize(lowerGesture).get("gestureName"));
+    }
+
+
 }
