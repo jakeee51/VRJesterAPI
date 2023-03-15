0095e78978d02b22b16239afdd3ff476a824f782
Jake Morfe <jakemorfe@gmail.com>
2023-03-14 20:58:35 -0400

    Minecraft 1.19.3 migration (init)
    currently can build 0.5.3

--- src/main/java/com/calicraft/vrjester/tracker/PositionTracker.java
+++ src/main/java/com/calicraft/vrjester/tracker/PositionTracker.java
@@ -1,8 +1,8 @@
 package com.calicraft.vrjester.tracker;
 
+import net.blf02.forge.VRAPIPlugin;
+import net.blf02.forge.VRAPIPluginProvider;
 import net.blf02.vrapi.api.IVRAPI;
-import net.blf02.vrapi.api.VRAPIPlugin;
-import net.blf02.vrapi.api.VRAPIPluginProvider;
 import net.blf02.vrapi.api.data.IVRPlayer;
 
 @VRAPIPlugin
