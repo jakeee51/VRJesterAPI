44ceb634f43713254bee14b913254abb9e6368ea
Jake Morfe <jakemorfe@gmail.com>
2023-11-11 16:41:40 -0500

    remove unused import

--- src/main/java/com/calicraft/vrjester/VrJesterApi.java
+++ src/main/java/com/calicraft/vrjester/VrJesterApi.java
@@ -18,7 +18,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
