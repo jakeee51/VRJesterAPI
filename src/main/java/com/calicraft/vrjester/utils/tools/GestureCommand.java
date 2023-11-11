854d676877a80000bc69a4c886554c0fa79669ab
Jake Morfe <jakemorfe@gmail.com>
2023-11-11 16:59:06 -0500

    fixed and organized GestureCommand

--- src/main/java/com/calicraft/vrjester/utils/tools/GestureCommand.java
+++ src/main/java/com/calicraft/vrjester/utils/tools/GestureCommand.java
@@ -7,92 +7,65 @@ import com.mojang.brigadier.arguments.BoolArgumentType;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 
+import static com.calicraft.vrjester.VrJesterApi.LOGGER;
 import static com.calicraft.vrjester.VrJesterApi.VIVECRAFT_LOADED;
 
 public class GestureCommand {
     // Class for registering the '/gesture' command
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-        if (VIVECRAFT_LOADED) { // VR
-            // TODO - Fix record command
-            dispatcher.register(Commands.literal("gesture")
-                    .then(Commands.literal("record")
-                            .then(Commands.argument("value", BoolArgumentType.bool())
-                                    .executes(c -> {
-                                        TriggerEventHandler.oneRecorded = false;
-                                        boolean mode = BoolArgumentType.getBool(c, "value");
-                                        TriggerEventHandler.config.RECORD_MODE = mode;
-                                        Config.writeConfig(TriggerEventHandler.config);
-                                        if (mode)
-                                            TriggerEventHandler.sendDebugMsg("Record mode enabled.");
-                                        else
-                                            TriggerEventHandler.sendDebugMsg("Record mode disabled.");
-                                        return 1;
-                                    })
-                            )
-                            .executes(c -> {
-                                TriggerEventHandler.oneRecorded = true;
-                                TriggerEventHandler.config.RECORD_MODE = true;
-                                TriggerEventHandler.sendDebugMsg("Record mode enabled. Only the next gesture will be recorded.");
-                                return 1;
-                            })
-                    )
-                    .then(Commands.literal("reload")
-                            .executes(c -> {
-                                TriggerEventHandler.gestures.load();
-                                TriggerEventHandler.config = Config.readConfig();
-                                TriggerEventHandler.sendDebugMsg("Reloading gestures & config from files!");
-                                return 1;
-                            })
-                    )
-                    .then(Commands.literal("save")
-                            .executes(c -> {
-                                TriggerEventHandler.gestures.write();
-                                TriggerEventHandler.sendDebugMsg("Writing all gestures to file!");
-                                return 1;
-                            })
-                    )
-                    .executes(c -> {
-                        TriggerEventHandler.sendDebugMsg("Possible modes: record, reload, save");
-                        return 1;
-                    })
-            );
-        } else { // Non-VR
-            dispatcher.register(Commands.literal("gesture")
-                    .then(Commands.literal("record")
-                            .then(Commands.argument("value", BoolArgumentType.bool())
-                                    .executes(c -> {
-                                        TriggerEventHandler.oneRecorded = false;
-                                        boolean mode = BoolArgumentType.getBool(c, "value");
-                                        if (mode)
-                                            TriggerEventHandler.sendDebugMsg("Record mode enabled.");
-                                        else
-                                            TriggerEventHandler.sendDebugMsg("Record mode disabled.");
-                                        return 1;
-                                    })
-                            )
-                            .executes(c -> {
-                                TriggerEventHandler.oneRecorded = true;
-                                TriggerEventHandler.sendDebugMsg("Record mode enabled. Only the next gesture will be recorded.");
-                                return 1;
-                            })
-                    )
-                    .then(Commands.literal("reload")
-                            .executes(c -> {
-                                TriggerEventHandler.sendDebugMsg("Reloading gestures from file!");
-                                return 1;
-                            })
-                    )
-                    .then(Commands.literal("save")
-                            .executes(c -> {
-                                TriggerEventHandler.sendDebugMsg("Writing all gestures to file!");
-                                return 1;
-                            })
-                    )
-                    .executes(c -> {
-                        TriggerEventHandler.sendDebugMsg("Possible modes: record, reload, save");
-                        return 1;
-                    })
-            );
-        }
+        dispatcher.register(Commands.literal("gesture")
+                .then(Commands.literal("record")
+                        .then(Commands.argument("value", BoolArgumentType.bool())
+                                .executes(c -> record(BoolArgumentType.getBool(c, "value")))
+                        )
+                        .executes(c -> record())
+                )
+                .then(Commands.literal("reload")
+                        .executes(c -> reload())
+                )
+                .then(Commands.literal("save")
+                        .executes(c -> save())
+                )
+                .executes(c -> {
+                    TriggerEventHandler.sendDebugMsg("Possible modes: record, reload, save");
+                    return 1;
+                })
+        );
+    }
+
+    private static int record(boolean mode) {
+        if (VIVECRAFT_LOADED)
+            TriggerEventHandler.config.RECORD_MODE = mode;
+        TriggerEventHandler.oneRecorded = false;
+        Config.writeConfig(TriggerEventHandler.config);
+        if (mode)
+            TriggerEventHandler.sendDebugMsg("Record mode enabled.");
+        else
+            TriggerEventHandler.sendDebugMsg("Record mode disabled.");
+        return 1;
+    }
+
+    private static int record() {
+        if (VIVECRAFT_LOADED)
+            TriggerEventHandler.config.RECORD_MODE = true;
+        TriggerEventHandler.oneRecorded = true;
+        Config.writeConfig(TriggerEventHandler.config);
+        TriggerEventHandler.sendDebugMsg("Record mode enabled. Only the next gesture will be recorded.");
+        return 1;
+    }
+
+    private static int reload() {
+        if (VIVECRAFT_LOADED)
+            TriggerEventHandler.gestures.load();
+        TriggerEventHandler.config = Config.readConfig();
+        TriggerEventHandler.sendDebugMsg("Reloading gestures & config from files!");
+        return 1;
+    }
+
+    private static int save() {
+        if (VIVECRAFT_LOADED)
+            TriggerEventHandler.gestures.write();
+        TriggerEventHandler.sendDebugMsg("Writing all gestures to file!");
+        return 1;
     }
 }
