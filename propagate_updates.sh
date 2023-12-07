ca24267cac9e4a2278ea0d0aebf291fb1367e7ee
Jake Morfe <jakemorfe@gmail.com>
2023-12-07 01:51:41 -0500

    finished mass update script

--- propagate_updates.sh
+++ propagate_updates.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-branches=("1.20.2")
-#branches=("1.18.2" "1.19.2" "1.19.3" "1.19.4" "1.20.1" "1.20.2")
-echo "Make sure there are no un-committed changes! Press Enter to continue..."
+branches=("1.18.2" "1.19.2" "1.19.3" "1.19.4" "1.20.1" "1.20.2")
+echo -e "Make sure there are no un-committed changes and that the commit is common (not unique per branch)\nPress Enter to continue..."
 read
 echo "Propagating commit updates..."
 
 for branch in "${branches[@]}"; do
   git checkout $branch
   git cherry-pick $1
+#  ./gradlew build # ensure `java -version` is 17+
   git push
 done
+
+echo "Updates were committed!"
